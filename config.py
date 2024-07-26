713e9096a90ac8d87480df1949651b058bac0a73
Lexie Marinelli <lexie.marinelli@gmail.com>
2024-07-26 13:47:39 -0600

    set up scrape and process, store document chunks into vectors

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 EMBEDDING_MODEL = 'text-embedding-ada-002'
+PG_COLLECTION_NAME = "landscape_rag"
+
--- scraper/scrape_and_process.py
+++ scraper/scrape_and_process.py
@@ -1,56 +1,73 @@
 import asyncio
+import os
 from asyncio import wait_for
 
 import dotenv
 from langchain_community.document_loaders import AsyncChromiumLoader, AsyncHtmlLoader, UnstructuredURLLoader, \
     SeleniumURLLoader, PlaywrightURLLoader
 from langchain_community.document_transformers import BeautifulSoupTransformer, Html2TextTransformer
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import PGVector
 from langchain_experimental.text_splitter import SemanticChunker
 
-from config import EMBEDDING_MODEL
+from config import EMBEDDING_MODEL, PG_COLLECTION_NAME
 from scraper.urls import urls
 
 dotenv.load_dotenv()
-
-
-async def load(loader):
-    return await loader.aload()
+url = ["https://www.waterwiseplants.org/find-a-plant/prickly-thrift/#header",
+       "https://www.waterwiseplants.org/find-a-plant/cockspur-hawthorn/#header",
+       "https://www.waterwiseplants.org/find-a-plant/cream-beauty-snow-crocus/#header"]
 
 
 async def read_url():
     # Load HTML
-    url = ["https://www.waterwiseplants.org/find-a-plant/prickly-thrift/#header"]
-    # loader = SeleniumURLLoader(["https://www.waterwiseplants.org/find-a-plant/"])
-    # loader = PlaywrightURLLoader(urls=url, remove_selectors=["header", "footer"])
-
-    loader = AsyncChromiumLoader(url)
-
-    doc = await load(loader)
+    # loader = SeleniumURLLoader(url)
+    # loader = AsyncChromiumLoader(url)
+    doc = await loader.aload()
     # print(doc)
     return doc
 
+# loop = asyncio.get_event_loop()
+# docs = loop.run_until_complete(wait_for(read_url(), 2000))
+# loop.close()
 
-loop = asyncio.get_event_loop()
-docs = loop.run_until_complete(wait_for(read_url(), 2000))
-loop.close()
+loader = PlaywrightURLLoader(urls=url, remove_selectors=["header", "footer"])
+docs = loader.load()
 
 # Transform
 # bs_transformer = BeautifulSoupTransformer()
-# docs_transformed = bs_transformer.transform_documents(docs, tags_to_extract=["div", "p"])
+# docs_transformed = bs_transformer.transform_documents(docs)
 html2text = Html2TextTransformer()
 docs_transformed = html2text.transform_documents(docs)
+# transformer = UnstructuredURLLoader
+# print(docs_transformed)
+# loader = UnstructuredURLLoader(
+#     urls=url, mode="elements", strategy="fast",
+# )
+# docs = loader.load()
+# # print(docs)
+
 embeddings = OpenAIEmbeddings(
     model=EMBEDDING_MODEL,
 )
 
 text_splitter = SemanticChunker(
-    embeddings=OpenAIEmbeddings()
+    embeddings=embeddings
 )
-
-chunks = text_splitter.split_documents(docs)
+#
+chunks = text_splitter.split_documents(docs_transformed)
 print(chunks)
 
 # Result
-print(len(docs_transformed))
-print(docs_transformed[0].page_content)
+# print(len(docs_transformed))
+# print(docs_transformed[0].metadata["source"])
+
+PGVector.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    collection_name=PG_COLLECTION_NAME,
+    connection_string=os.getenv("POSTGRES_URL"),
+    pre_delete_collection=True
+)
+
+
